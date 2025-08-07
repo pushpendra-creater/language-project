64098770ac46a927de23a4e34155d0dba0b62fd9
pushpendra-creater <patilpushpendra2005@gmail.com>
2025-08-07 11:58:02 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+package school;
+public class Student
+{
+   public String name;
+   public String dob;
+   public int rollno;
+   public void show()
+   {
+      System.out.println("Name="+this.name);
+      System.out.println("DOB="+this.dob);
+	  System.out.println("RollNumber="+this.rollno);
+   }
+}	
